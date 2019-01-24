2a4c36537b547ed9212a08abefa9e7262c784e77
Melissa Sage Dumont <melissadumont91@gmail.com>
2019-01-24 14:42:16 -0500

    Solved SICP 1.3

---
+++ sicp/1.3.scm
@@ -0,0 +1,9 @@
+(define (square x) (* x x))
+
+(define (sumsquares x y) (+ (square x) (square y)))
+
+(define (sqsumlargest a b c)
+  (cond
+    ((and (>= a c) (>= b c)) (sumsquares a b))
+    ((and (>= b a) (>= c a)) (sumsquares b c))
+    ((and (>= a b) (>= c b)) (sumsquares a c))))
